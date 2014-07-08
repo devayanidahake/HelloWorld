9d658c1353de9c832022af9eee523e5d06b94757
niketkadde <niketkadde@gmail.com>
2014-07-08 16:07:38 +0530

    HelloWorld
    First Commit

---
+++ gen/com/nik/helloworld/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.nik.helloworld;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/nik/helloworld/MainActivity.java
@@ -0,0 +1,24 @@
+package com.nik.helloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		//Originally added by niket.
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
